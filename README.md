d0c82bcb4792ba5f64d3daa0cc8c122cfb359dc3
Md Samsuzzoha <mdshayon0@gmail.com>
2023-01-07 23:00:54 +0600

    delete single photo

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Springboot
  - [tutorial](https://www.youtube.com/watch?v=QuvS_VLbGko), [start project](https://start.spring.io/) from here and import into intellij or eclipse
  - There are 3 api endpoints is beed made
-    - `/` Test the api
-    - `/photoz` Get all photo
-    - `/photoz/1` Get single photo
+    - `/` Test the api `curl -X DELETE http://localhost:8080`
+    - `/photoz` Get all photo `curl -X GET http://localhost:8080/photoz`
+    - `/photoz/1` Get single photo `curl -X GET http://localhost:8080/photoz/1`
+    - `/photoz/1` delete single photo `curl -X DELETE http://localhost:8080/photoz/1`
+    - 
+   
--- src/main/java/com/tfl/shayon/springboot/photoz/PhotozController.java
+++ src/main/java/com/tfl/shayon/springboot/photoz/PhotozController.java
@@ -1,6 +1,7 @@
 package com.tfl.shayon.springboot.photoz;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,11 @@ public class PhotozController {
         Photo photo = db.get(id);
         if(photo == null ) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return photo;
+    }
+
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable String id){
+        Photo photo = db.remove(id);
+        if(photo == null ) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
