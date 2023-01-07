9b5464607f61f2a30aa21b5c9205ba80b9e52200
Md Samsuzzoha <mdshayon0@gmail.com>
2023-01-07 23:12:07 +0600

    create a photo with post request

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
     - `/photoz` Get all photo `curl -X GET http://localhost:8080/photoz`
     - `/photoz/1` Get single photo `curl -X GET http://localhost:8080/photoz/1`
     - `/photoz/1` delete single photo `curl -X DELETE http://localhost:8080/photoz/1`
+    - `/photoz` create a photo `curl -X POST http://localhost:8080/photoz -H 'Content-Type: application/json' -d '{"fileName": "file2.jpg"}'`
     - 
    
--- src/main/java/com/tfl/shayon/springboot/photoz/PhotozController.java
+++ src/main/java/com/tfl/shayon/springboot/photoz/PhotozController.java
@@ -1,16 +1,10 @@
 package com.tfl.shayon.springboot.photoz;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class PhotozController {
@@ -42,5 +36,12 @@ public class PhotozController {
     public void delete(@PathVariable String id){
         Photo photo = db.remove(id);
         if(photo == null ) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestBody Photo photo){
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 }
