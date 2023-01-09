9111458d7d5b4717f59275b7a6119580689d7aab
Md Samsuzzoha <mdshayon0@gmail.com>
2023-01-09 17:21:38 +0600

    clean up for micro services

--- src/main/java/com/tfl/shayon/springboot/photoz/repository/PhotozRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tfl.shayon.springboot.photoz.repository;
-
-import com.tfl.shayon.springboot.photoz.model.Photo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhotozRepository extends CrudRepository<Photo, Integer> {
-
-}
