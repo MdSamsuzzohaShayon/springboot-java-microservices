54ffca0d9cca91b8e7d69bd4db65bebeb370287a
Md Samsuzzoha <mdshayon0@gmail.com>
2023-01-15 04:43:30 +0600

    clean up

--- order-service/src/main/java/com/tss/shayon/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tss.shayon.orderservice.repository;
-
-import com.tss.shayon.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
