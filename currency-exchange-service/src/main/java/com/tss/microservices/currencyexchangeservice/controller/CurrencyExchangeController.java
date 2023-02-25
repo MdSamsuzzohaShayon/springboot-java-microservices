d9dfe12261484212ceca81d053891fed7d90e02a
Md Shayon <mdshayon0@gmail.com>
2023-02-25 19:12:39 +0600

    141S12 - create multiple instance and run as java application without spring boot tools in eclipse

--- currency-exchange-service/src/main/java/com/tss/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/tss/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.tss.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,15 @@ import com.tss.microservices.currencyexchangeservice.currencyexchange.CurrencyEx
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
+	
 //	curl http://localhost:8000/currency-exchange/from/BDT/to/USD
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from ,@PathVariable String to) {
-		return new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 	}
 }
