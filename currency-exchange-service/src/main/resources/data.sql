909017a70751968fb331fe3a740ca34d07aaf99b
Md Shayon <mdshayon0@gmail.com>
2023-02-25 19:31:45 +0600

    142S13 - create entity and taable using jpa and hibernate

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(10001, 'USD', 'BDT', 65, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(10002, 'EUR', 'BDT', 89, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(10003, 'AUD', 'BDT', 45, '');
